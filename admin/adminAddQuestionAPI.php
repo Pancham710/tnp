37125e2fe10391c6e9127da465b29f47441119fa
syntax-error-1 <patilchitrangad1@gmail.com>
2023-03-02 14:48:39 +0530

    asd

--- admin/adminAddQuestionAPI.php
+++ admin/adminAddQuestionAPI.php
@@ -7,7 +7,7 @@ include "../connection.php";
 $companyid = $_POST["companyid"];
 $questions = $_POST["questions"];
 if(strlen($companyid) > 0 && strlen($questions) > 0){
-    $sql_query = "UPDATE questionbank SET Questions='questions' WHERE id=".$companyid."";
+    $sql_query = "UPDATE questionbank SET Questions= '".$questions."' WHERE id=".$companyid."";
 
     if ($conn->query($sql_query) === TRUE) {
         header('location: ./adminUpdateQuestionBank.php');
